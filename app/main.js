cd0c08b473ed36b35b34bae757be88b84a556238
Bo <bo.robbrecht@student.howest.be>
2021-03-09 15:33:03 +0100

    Able to reach /users/ endpoint

--- app/main.js
+++ app/main.js
@@ -2,7 +2,12 @@
 const ops = require('./init')
 const port = 8888
 
+//Express Routers
+const users = require('./routers/userRouter')
+
 ops.server.listen(port, ()=> {
     console.log(`Server is running on ${port}`)
 })
+
+ops.app.use('/users', users);
 
