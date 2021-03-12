dc1a6a226467839c5e7b9ca9c0dff734e1d4f8e2
Bo <bo.robbrecht@student.howest.be>
2021-03-12 12:15:57 +0100

    Able to implement OpenAPIValidator. This means that the Body, parameters, queries and request bodies are automatically validated.

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 //Node Constants
 const ws = require('./config/ws.js')
+const OpenApiValidator = require('express-openapi-validator');
 require('colors')
 
 //Express Routers
@@ -8,15 +9,31 @@ const users = require('./data/users/router')
 init()
 
 function init() {
-    ws.server.listen(ws.port, () => {
-        console.log("Server is running on", `${ws.port}`.yellow)
-        initRouters()
-        console.log("API server is", "operational.".green)
-    })
+    /*Set up parsers*/
     ws.app.use(ws.express.json());
     ws.app.use(ws.express.urlencoded({extended: false}))
+    /*Set up validator*/
+    ws.app.use(OpenApiValidator.middleware({
+        apiSpec: './app/openapi.yaml',
+        validateRequests: true,
+        validateApiSpec: true
+    }))
+    /*Set up Validator Handler*/
+    ws.app.use((err, req, res, next) => {
+        res.status(err.status || 500).json({
+            message: err.message,
+            errors: err.errors,
+        });
+        console.log(`${err.status}`.red, `${req.method}`.bold, `${req.url} :  `.bold, `${err.message}`.red)
+    });
+    /*Start routers*/
+    initRouters()
+    console.log("Express is", "configured.".green)
+    /*Start Webserver*/
+    ws.server.listen(ws.port, () => {
+        console.log("Server is running on", `${ws.port}`.yellow)
+    })
 }
-
 
 function initRouters() {
     ws.app.use('/users', users);
