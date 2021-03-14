ff0ba30db3a90edd21f77d2bec213690dfd65de9
Bo <bo.robbrecht@student.howest.be>
2021-03-14 16:29:26 +0100

    Made encrypt.js, small cli application to manually hash passwords.

---
+++ app/encrypt.js
@@ -0,0 +1,15 @@
+const bcrypt = require('bcrypt')
+const cmd = require('command-line-args')
+require('colors')
+
+const optionDefinitions = [
+    {name: 'input', alias: "i", type: String}
+]
+const saltRounds = 10
+const options = cmd(optionDefinitions)
+
+bcrypt.hash(options.input, saltRounds).then((hash, err) =>
+    (err === null)
+        ? console.log(`Unable to hash input:`.bold.red, `${err}`)
+        : console.log(`${options.input}'s hash is:\n`.underline, `${hash}`.yellow)
+)
