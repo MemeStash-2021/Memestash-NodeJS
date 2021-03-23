ff89b943e7710b5d76e715beb23efd01b5931c60
Bo <bo.robbrecht@student.howest.be>
2021-03-23 09:30:28 +0100

    Created statement & updated null references for picture

--- app/data/users/statements.js
+++ app/data/users/statements.js
@@ -1,5 +1,9 @@
 module.exports = {
     getUsers: 'SELECT * FROM memestash.users;',
     getUsersFiltered: 'SELECT * FROM memestash.users where username LIKE ?;',
-    addUser: 'INSERT INTO memestash.users(username, email, password, wallet) Values (?,?,?,wallet)'
+    addUser: 'INSERT INTO memestash.users(username, email, password, wallet) Values (?,?,?,wallet)',
+    getUser: `SELECT users.*, cards.* FROM memestash.users u
+                JOIN memestash.collections co ON u.id = co.user_id
+                JOIN memestash.cards ca ON co.card_id = ca.id
+                WHERE user_id = ?;`
 }
