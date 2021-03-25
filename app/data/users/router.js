e750973cb500fc9730cd1399507a508b5b042d6b
Bo <bo.robbrecht@student.howest.be>
2021-03-25 18:40:44 +0100

    Added mock responses for PUT /users/{ouid}/chats/{tuid}

--- app/data/users/router.js
+++ app/data/users/router.js
@@ -226,6 +226,49 @@ router.route("/:ouid/chats/:tuid")
 			});
 		}
 		res.json(json);
+	}))
+	// TODO: Don't forget to implement the actual query parameters
+	.patch((req, res) => {
+		const ouid = parseInt(req.params.ouid), tuid = parseInt(req.params.tuid), message=req.body.message;
+		let json = [];
+		for(let i = 0; i<10; i++){
+			json.push({
+				message: (i === 0) ? message : "This is a example message",
+				date: `2021-03-2${5-i}T17:13:20.599Z`,
+				sender: (i % 2 === 0) ? "Mori" : "Ruiner",
+				senderId: (i % 2 === 0) ? ouid : tuid
+			});
+		}
+		res.json({
+			user: {
+				id: ouid,
+				name: "Ruiner"
+			},
+			correspondent: {
+				id: tuid,
+				name: "Mori"
+			},
+			messages: json
+		});
+	})
+	.put(((req, res) => {
+		const ouid = parseInt(req.params.ouid), tuid = parseInt(req.params.tuid), message=req.body.message;
+		res.json({
+			user: {
+				id: ouid,
+				name: "Ruiner"
+			},
+			correspondent: {
+				id: tuid,
+				name: "Mori"
+			},
+			messages: {
+				message: message,
+				date: "2021-03-25T17:13:20.599Z",
+				sender: "Mori",
+				senderId: ouid
+			}
+		});
 	}));
 
 module.exports = router;
