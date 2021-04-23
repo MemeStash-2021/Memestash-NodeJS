1ee4338163602ba5e1bda58c41d4c57668cc496d
Bo <bo.robbrecht@student.howest.be>
2021-04-23 10:52:40 +0200

    Replaced logging in users/router with new logger

--- app/data/util/logger.js
+++ app/data/util/logger.js
@@ -1,17 +1,17 @@
 "use strict";
 
-exports.log200 = (method, address) => {
-	console.log("200".yellow, `${method} ${address}`.bold, ": ", "OK".bold.green);
+exports.log200 = (req) => {
+	console.log("200".yellow, `${req.method} ${req.baseUrl + req.url}`.bold, ": ", "OK".bold.green);
 };
 
-exports.log201 = (method, address) => {
-	console.log("201".yellow, `${method} ${address}`.bold, ": ", "Created".bold.green);
+exports.log201 = (req) => {
+	console.log("201".yellow, `${req.method} ${req.baseUrl + req.url}`.bold, ": ", "Created".bold.green);
 };
 
-exports.log404 = (method, address, message) => {
-	console.log("404".red, `${method} ${address}`.bold, ": ", message);
+exports.log404 = (req, message) => {
+	console.log("404".red, `${req.method} ${req.baseUrl + req.url}`.bold, ": ", message);
 };
 
-exports.log500 = (method, address) => {
-	console.log("500".bold.red, `${method} ${address}`.bold, ": ", "Internal Server Error".bold.bgRed.white);
+exports.log500 = (req) => {
+	console.log("500".bold.red, `${req.method} ${req.baseUrl + req.url}`.bold, ": ", "Internal Server Error".bold.bgRed.white);
 };
